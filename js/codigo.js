791f597a4bcfcdeb90df5851e434ea9e8ef1a443
hydrapain1 <romanadr2019@gmail.com>
2019-05-22 13:16:58 +0200

    Funciones para validar nombre, teléfono y ciudad. RRM

---
+++ js/codigo.js
@@ -0,0 +1,26 @@
+function validarTelefono() {
+	var campoTelefono = document.getElementById('campoTelefono');
+	if (/^\d{9}$/.test(campoTelefono.value)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function validarNombre() {
+	var campoNombre = document.getElementById('campoNombre');
+	if (/^[a-zA-Z ]{2,30}$/.test(campoNombre.value)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function validarCiudad() {
+	var campoCiudad = document.getElementById('campoCiudad');
+	if (/^[a-zA-Z ]{2,30}$/.test(campoCiudad.value)) {
+		return true;
+	} else {
+		return false;
+	}
+}
